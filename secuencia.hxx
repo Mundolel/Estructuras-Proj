ef22973de69ad4d66cf6ba020b16a876f4d19a40
CataAceros <macaaceros@gmail.com>
2025-09-03 14:47:10 -0500

    Update secuencia.hxx

--- secuencia.hxx
+++ secuencia.hxx
@@ -2,36 +2,29 @@
 #define SECUENCIA_HXX
 
 #include "secuencia.h"
-
-// Constructores
-Secuencia::Secuencia()
-    : name(""), code() {
+// Constructor por defecto
+Secuencia::Secuencia() {
+    name = "";
+    code.clear();
+    ancho = 0;
 }
 
-Secuencia::Secuencia(const string& nombre, const list<char>& bases)
-    : name(nombre), code(bases) {
+// Constructor con parámetros (todo por valor)
+Secuencia::Secuencia(string nombre, list<char> bases, int a) {
+    name = nombre;  // copia el valor
+    code = bases;   // copia la lista
+    ancho = a;      // valor dado por el usuario
 }
 
-// Getters (POR VALOR)
-string Secuencia::getName() const {
-    return name;
-}
+// Getters
+string Secuencia::getName() const { return name; }
+list<char> Secuencia::getCode() const { return code; }
+int Secuencia::getAncho() const { return ancho; }
 
-list<char> Secuencia::getCode() const {
-    return code;
-}
-
-
-
-// Setters (reciben por VALOR)
-void Secuencia::setName(string n) {
-    name = n;
-}
-
-void Secuencia::setCode(list<char> c) {
-    code = c;
-}
-
+// Setters
+void Secuencia::setName(string n) { name = n; }
+void Secuencia::setCode(list<char> c) { code = c; }
+void Secuencia::setAncho(int a) { ancho = a; }
 
 
 #endif
