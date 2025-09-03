5fd45e5eb7ad179711c35eff90834b17a5dd0f24
CataAceros <macaaceros@gmail.com>
2025-09-03 14:32:22 -0500

    Update sistema.hxx

--- sistema.hxx
+++ sistema.hxx
@@ -12,6 +12,9 @@ using namespace std;
 Sistema() {
         list_secuencia.clear(); // la deja vacía
     }
+bool estaVacia const {
+        return list_secuencia.empty();
+    }
 
 void Sistema::cargar(std::string nombreArchivo) { 
     list_secuencia.clear(); // borrar lo anterior
